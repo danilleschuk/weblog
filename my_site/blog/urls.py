3ad9c299efd74c468647b9d0c5675926452460ff
danil <danillesuk6766@gmail.com>
2025-03-21 02:43:53 +0300

    Добавил комментарии и рекомендации на основе тегов

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path(
         '<int:post_id>/comment/', views.post_comment, name='post_comment'
     ),
+    path(
+        'tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'
+),
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Post
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 from .forms import CommentForm
+from .models import Post
+from taggit.models import Tag
 
-def post_list(request):
+
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
+
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
@@ -18,7 +28,8 @@ def post_list(request):
 
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                        'tag': tag  })
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -29,8 +40,17 @@ def post_detail(request, year, month, day, post):
         publish__month = month,
         publish__day = day
     )
+
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = (Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id))
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
 
     return render(
         request,
@@ -38,7 +58,8 @@ def post_detail(request, year, month, day, post):
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
         }
     )
 
